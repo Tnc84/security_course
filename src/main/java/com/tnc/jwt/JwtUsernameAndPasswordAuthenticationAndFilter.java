d1e21d3d0cadf5e9f03318612b01657c159e7df6
TncBogdan <tncbogdan@gmail.com>
2021-07-14 06:23:51 +0300

    add token in  JwtUsernameAndPasswordAuthenticationAndFilter class

--- src/main/java/com/tnc/jwt/JwtUsernameAndPasswordAuthenticationAndFilter.java
+++ src/main/java/com/tnc/jwt/JwtUsernameAndPasswordAuthenticationAndFilter.java
@@ -1,15 +1,21 @@
 package com.tnc.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class JwtUsernameAndPasswordAuthenticationAndFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -36,5 +42,19 @@ public class JwtUsernameAndPasswordAuthenticationAndFilter extends UsernamePassw
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String key = "securesecuresecuresecuresecuresecure";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new java.util.Date())
+                .setExpiration(Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+        response.addHeader("Authorization", "Bearer" + token);
     }
 }
